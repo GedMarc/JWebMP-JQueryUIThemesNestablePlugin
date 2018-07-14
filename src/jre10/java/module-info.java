edaf22a7672314f79c9857f9218aeea0645cef6c
GedMarc <ged_marc@hotmail.com>
2018-07-14 03:43:42 +0200

    JDK 10 Structure and Configuration

---
+++ src/jre10/java/module-info.java
@@ -0,0 +1,11 @@
+module com.jwebmp.plugins.jqueryui {
+
+	exports com.jwebmp.plugins.jqueryui.themes;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.logmaster;
+	requires com.fasterxml.jackson.annotation;
+
+	requires java.validation;
+	requires java.logging;
+}
