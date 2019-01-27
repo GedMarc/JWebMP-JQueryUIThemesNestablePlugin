d9633e6928a8cde37e2f1fa37645ed6a639ea2c1
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:32 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,9 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.jqueryui.themesnested.JQUINestableThemesPageConfigurator;
-import com.jwebmp.plugins.jqueryui.themesnested.implementations.JQueryUINestableThemesExclusionsModule;
-
 module com.jwebmp.plugins.jqueryui.themes.nestable {
 
 	exports com.jwebmp.plugins.jqueryui.themesnested;
@@ -16,10 +10,10 @@ module com.jwebmp.plugins.jqueryui.themes.nestable {
 	requires java.logging;
 	requires com.jwebmp.guicedinjection;
 
-	provides IPageConfigurator with JQUINestableThemesPageConfigurator;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.jqueryui.themesnested.JQUINestableThemesPageConfigurator;
 
-	provides IGuiceScanJarExclusions with JQueryUINestableThemesExclusionsModule;
-	provides IGuiceScanModuleExclusions with JQueryUINestableThemesExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.jqueryui.themesnested.implementations.JQueryUINestableThemesExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.jqueryui.themesnested.implementations.JQueryUINestableThemesExclusionsModule;
 
 	opens com.jwebmp.plugins.jqueryui.themesnested to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
