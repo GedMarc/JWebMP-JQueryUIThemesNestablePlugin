cb82974d09857dfa5f331af3ca8bee9f764ff16a
Marc Magon <marc.magon@gmail.com>
2024-02-04 23:41:46 +0200

    Updates for version 2

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -5,7 +5,7 @@ module com.jwebmp.plugins.jqueryui.themes.nestable {
 	exports com.jwebmp.plugins.jqui.themesnested;
 
 	requires com.jwebmp.core;
-	requires com.guicedee.logmaster;
+	
 
 	requires jakarta.validation;
 	requires java.logging;
